f6cb4b43944e98432c0da630004239f5b146687d
takata-k <takata-k@illmatics.co.jp>
2025-04-05 23:43:12 +0900

    update commit

---
+++ app/src/main/java/com/example/myapplication/BlankFragment.kt
@@ -0,0 +1,91 @@
+package com.example.myapplication
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.appcompat.app.AlertDialog
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BlankFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
+
+interface DialogResultListener {
+    fun onDialogResult(isConfirmed: Boolean)
+}
+
+class MyDialogFragment : DialogFragment() {
+    // ダイアログを作成するためにonCreateDialogをオーバーライド
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // AlertDialogを使ってダイアログを作成
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("This is a DialogFragment")
+                .setPositiveButton("OK") { dialog, id ->
+                    // OKボタンが押された場合、結果を返す
+                    (activity as? DialogResultListener)?.onDialogResult(true)
+                }
+                .setNegativeButton("Cancel") { dialog, id ->
+                    // Cancelボタンが押された場合、結果を返す
+                    (activity as? DialogResultListener)?.onDialogResult(false)
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,11 +7,11 @@ import android.widget.Toast
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DialogResultListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.calculate_button)
@@ -20,17 +20,44 @@ class MainActivity : AppCompatActivity() {
         // ボタンがクリックされた時の処理
         button.setOnClickListener {
             val enteredText = editText.text.toString()
-            Toast.makeText(this, "「$enteredText」", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "「$enteredText」", Toast.LENGTH_SHORT).show()
             Log.v("テスト", "これはメッセージです「$enteredText」終わり")
-            Log.d("テスト", "これはメッセージです「$enteredText」終わり")
-            Log.i("テスト", "これはメッセージです「$enteredText」終わり")
-            Log.w("テスト", "これはメッセージです「$enteredText」終わり")
-            Log.e("テスト", "これはメッセージです「$enteredText」終わり")
-            Log.d("MainActivity", "Button Clicked!")
+//            Log.d("テスト", "これはメッセージです「$enteredText」終わり")
+//            Log.i("テスト", "これはメッセージです「$enteredText」終わり")
+//            Log.w("テスト", "これはメッセージです「$enteredText」終わり")
+//            Log.e("テスト", "これはメッセージです「$enteredText」終わり")
+//            Log.d("MainActivity", "Button Clicked!")
+
+//            showFragment()
+            showDialog()
         }
     }
 
     override fun onResume() {
         super.onResume()
+    }
+
+    private fun showDialog() {
+        val dialogFragment = MyDialogFragment()
+        dialogFragment.show(supportFragmentManager, "dialog_tag")
+    }
+
+    private fun showFragment() {
+        val fragment = BlankFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, fragment)  // 置き換えるビューIDを指定
+        transaction.addToBackStack(null);
+        transaction.commit()
+    }
+
+    // ダイアログから結果を受け取る
+    override fun onDialogResult(isConfirmed: Boolean) {
+        if (isConfirmed) {
+            // OKが選ばれたとき
+            println("User confirmed.")
+        } else {
+            // Cancelが選ばれたとき
+            println("User canceled.")
+        }
     }
 }
